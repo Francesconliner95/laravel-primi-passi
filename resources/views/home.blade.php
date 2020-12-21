39a37d1c7696db6b81d534b3f575ac2d341daefb
Francesconliner <francescodivincenzo95@outlook.it>
2020-12-21 16:11:54 +0100

    Test
    Link, sass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,11 @@
     <head>
         <meta charset="utf-8">
         <title></title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <h1>home</h1>
+        <a href="{{ route('pag_test') }}">Pagina di prova collegamento 1</a>
+        <a href="{{ url('/test') }}">Pagina di prova collegamento 2</a>
     </body>
 </html>
